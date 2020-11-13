66f68e1bfcba1da45d55cdff762c01384beacb03
asandy1023 <65816973+asandy1023@users.noreply.github.com>
2020-11-13 22:23:42 +0800

    2
    2

--- snake.asm
+++ snake.asm
@@ -6,7 +6,7 @@
 #	Display Height in Pixels: 512
 #	Base address for display: $gp
 #---------------------------------------------------------------------------
-#	$s0	snakeaddress address
+#	$s0	snakeaddr address
 #	$s1	futureheadaddress
 #	$s2	newFirstbody or oldheadaddress
 #	$s3	bodylength
@@ -18,115 +18,106 @@
 ##########################################################
 .data
 	outstring: .asciiz "Snake l= "
-	#check_error: .asciiz	"OK\n"
 	newline: .asciiz "\n"
 	side: .half 32			#Give a length 32 of a bitmap
 	black: .half 0x0
 	growupflag: .half 1
 	vertical:	.half 30
 	horizontal:	.half 30
-	check_error:	.half 	0
 	contourcolor: .word 0xabcdef
 	foodcolor: .word 0xCC0000
 	snakecolor: .word 0xAAAAAAAA
 	bodylength: .half 3
-	#growupflag: .word 0x01
 	startaddress: .word 0x10008000
-	snakeaddress: .word 0x0	 	#snakeaddress array
+	snakeaddr: .word 0x0	 	#snakeaddress array
 	
 	
 
 	
 
-##########################################################################	
+###########################################################
 .text
 init:			
 	li $t0, 0x1000819c		#Assign snake address to $t0
-	sw $t0, snakeaddress		#Store snake address from $t0 to "snakeaddress"
-	jal clear			#Call clear function
-	lw $t0, startaddress		#Load display start address
-	jal row				#Call the up row function
-	jal columns			#Call the column function
-	jal row				#Call the bottom row funtion
-	jal snake			#Call the snake funtion
-	jal food			#Call the food funtion	
+	sw $t0, snakeaddr		#$t0 = "snakeaddress"
+	jal clear			
+	lw $t0, startaddress		#$t0 = start address
+	jal row				
+	jal columns			
+	jal row				
+	jal snake			
+	jal food			
 	lw $t9,snakecolor
-	li $t7,0			#use to check eat self (is a flag)
-	li $t6,15			#use to check infinity loop
-	li $v1,1			#進入安全模式前的預設動作
+	li $t7,0			#檢查會不會自殘
+	li $t6,15		#檢查會不會進入無限迴圈
 	li $k0,0			#用於確認前半圈是否有吃到食物
 	li $k1,0			#用於確認後半圈是否有吃到食物
-################################################################	
-headXY:					#get head origin coordinate(if snake alive ,never change)
-	la $s0,snakeaddress
+################################################
+FindheadXY:			#找蛇頭
+	la $s0,snakeaddr
 	lw $s1,($s0)
 	subi $t0,$s1,0x10008000
 	div $t0,$t0,0x80
 	mfhi $t0
-	mflo $s5                         #get headY
-	div $s4,$t0,4			 #get headX
-	######################################### Security Mode ###############################################
-StrategyS:
-	li $a0,10			#Decrease to increase difficulty; Increase to decrease difficulty
-	li $v0,32			#Sleep for 60 milliseconds
+	mflo $s5                       #headY
+	div $s4,$t0,4		 #headX
+###############################################Security Mode 
+SecurityS:
+	li $a0,10			#調節速度; Increase to decrease difficulty
+	li $v0,32			#Sleep for 16 ms
 	syscall
 	
 	li $t0,1			
-	beq $s5,$t0,Loop1		#若現在蛇在最上層則進入安全mode
-	beq $v1,$zero,Loop1		#代表進入security模式($v1==1時，蛇會先想辦法先走到上邊界，而不執行security mode)
-	addi $s5,$s5,-1
+	beq $s5,$t0,Lp1		#若現在蛇在最上層則進入Security mode
+	j Lp1		
 	j upS
 DirectionS:
-#=========================================================================
-Loop1:					#(貼著上邊界走)
-	li $v1,0			#v1==0代表蛇正式進入security mode
-	bne $a2,$0,Loop2		#若上邊界還沒走完，$a2==0
+#================================================================
+Lp1:					#(貼著上邊界走)
+	bne $a2,$0,Lp2		#若上邊界還沒走完，$a2==0
 	slti $t0,$s4,30			#X軸最遠只能到30，判斷是否已走到底
-	beq $t0,$0,Loop2		#若已走到底，則跳到loop2
-	bne $k0,$0,LL1			#k0==1代表現在是可以吃食物的狀態，$k0==0則代表剛剛已吃到食物，
-					#並讓蛇回到邊框(以最短路徑)，不讓蛇繼續吃食物
-	addi $s4,$s4,1
+	beq $t0,$0,Lp2		#若已走到底，則跳到loop2
+	bne $k0,$0,LL1		#k0==1代表現在是可以吃食物的狀態，$k0==0則代表剛剛已吃到食物，
+					#並讓蛇回到邊框，不讓蛇吃食物了	
+addi $s4,$s4,1
 	j rightS
 
 LL1:	
-	beq $s4,$s6,Loop2		#若現在蛇的位置(hx)和食物(foodx)相同，則跳至loop2(往下走)
+	beq $s4,$s6,Lp2		#若現在蛇的位置和食物x軸相同，goto loop2(往下走)
 	addi $s4,$s4,1			
 	j rightS
-#=========================================================================	
-Loop2:					#(貼著右邊界走or為了找食物而往下走)
-	li $v1,0			#v1==0代表蛇正式進入security mode
+#===============================================================
+Lp2:					#(貼著右邊界走or為了找食物而往下走)
 	li $a2,1			#代表上邊界走完，$a2==1
-	bne $a3,$0,Loop3		#若右邊界還沒走完，$a3==0
+	bne $a3,$0,Lp3		#若右邊界還沒走完，$a3==0
 	slti $t0,$s5,30			#y軸最遠只能到30，判斷是否已走到底
-	beq $t0,$0,Loop3		#若已走到底，則跳到loop3
-	#beq $s5,$s7,ck
+	beq $t0,$0,Lp3		#若已走到底，則跳到loop3
 	bne $k0,$0,LL2			#k0==1代表現在是可以吃食物的狀態，$k0==0則代表剛剛已吃到食物
 	addi $s5,$s5,1
 	j underS
 LL2:
-	beq $s5,$s7,ckk1		#若現在蛇的位置(hy)和食物(foody)相同，則跳至ckk1代表已經吃到食物
+	beq $s5,$s7,skr1		#若現在蛇的位置和食物y軸相同，則跳至skr1代表已經吃到食物
 	addi $s5,$s5,1			#若沒有則繼續往下走
 	j underS			
-ckk1:
+skr1:
 	li $a2,0			#設$a2==0是因為剛剛因為$k0==1時，在LL1判斷hx==foodx時就先跳到loop2(向下走)，
-					#而不是因為$s4已經走到30才往下走，所以在此要設$a2==0讓蛇把x軸走完(走到30)
-	li $k0,0			#因已經吃到食物，設$k0==0則代表剛剛已吃到食物
-	j Loop1
-#=========================================================================
-Loop3:					#(貼著下邊界走)
-	li $v1,0			#v1==0代表蛇正式進入security mode
+					#而不是因為$s4已經走到30才往下走，所以要設$a2==0讓蛇把x軸走到30
+	li $k0,0			#因已經吃到食物，設$k0==0
+	j Lp1
+#================================================================
+Lp3:					#(貼著下邊界走)
 	li $a3,1			#代表右邊界走完，$a3==1
 	slti $t0,$s4,2			#判斷hx是否以走到下邊界的最左邊(x==1)
-	bne $t0,$zero,Loop4		#若有則跳至loop4
+	bne $t0,$zero,Lp4		#若有則跳至loop4
 	bne $k1,$0,LL3			#k1==1代表現在是可以吃食物的狀態，$k1==0則代表剛剛已吃到食物
 	subi $s4,$s4,1
 	j leftS
 LL3:
-	beq $s4,$s6,Loop4		#若現在蛇的位置(hx)和食物(foodx)相同，則跳至loop4(往上走)
+	beq $s4,$s6,Lp4		#若現在蛇的位置和食物x軸相同，則跳至loop4(往上走)
 	subi $s4,$s4,1
 	j leftS
-#=========================================================================
-Loop4:					#(貼著左邊界走)
+#==============================================================
+Lp4:					#(貼著左邊界走)
 	slti $t0,$s5,2			#判斷hy是否以走到左邊界的最上面(y==1)
 	bne $t0,$0,check		#若有則跳至check做重設的動作
 	bne $k1,$0,LL4			#k1==1代表現在是可以吃食物的狀態，$k1==0則代表剛剛已吃到食物
@@ -134,13 +125,13 @@ Loop4:					#(貼著左邊界走)
 	j upS
 	
 LL4:
-	beq $s5,$s7,ckk2		#若現在蛇的位置(hy)和食物(foody)相同，則跳至ckk2代表已經吃到食物
+	beq $s5,$s7,skr2		#若現在蛇的位置(hy)和食物(foody)相同，則跳至skr2代表已經吃到食物
 	subi $s5,$s5,1
 	j upS
-ckk2:	
+skr2:	
 	li $k1,0			#因已經吃到食物，設$k1==0則代表剛剛已吃到食物
-	j Loop3				#在此要讓蛇把x軸走完(走到1)
-#=========================================================================
+	j Lp3				#在此要讓蛇把x軸走完(走到1)
+#============================================================
 check:					#(繞完一圈旗標重設)
 	li $a2,0			#用於確認上邊界是否走完
 	li $a3,0			#用於確認右邊界是否走完
@@ -150,21 +141,20 @@ check:					#(繞完一圈旗標重設)
 	
 #################################################################	
 gettailXY:				#get tailXY
-	la $s0,snakeaddress		
+	la $s0,snakeaddr		
 	lw $s3,bodylength		
-	li $t3,30			#check bodylength biger than 30 
-	beq $s3,$t3,StrategyS		#if bodylength>30 goto StrategS (Security mode)
+	j SecurityS		#goto StrategS
 	addi $s3,$s3,-1	
-	sll $s3,$s3,2			#operate tail coordinate in address: (bodylength-1)*4+first snakeaddress
+	sll $s3,$s3,2			#在地址中操作尾坐標：（bodylength-1）* 4 + first snakeaddr
 	add $t2,$s0,$s3			
 	lw $t1,($t2)			#get tail address
-	subi $t0,$t1,0x10008000		#operate tail coordinate
+	subi $t0,$t1,0x10008000		#操作尾坐標
 	div $t0,$t0,0x80
 	mfhi $t0
 	mflo $t9                         #get tailY
 	div $t8,$t0,4			 #get tailX
 	#==========================================
-	la $s0,snakeaddress		#get this time head coordinate(always change)
+	la $s0,snakeaddr		#得到這次頭部坐標（總是會改變的）
 	lw $s1,($s0)
 	subi $t0,$s1,0x10008000		
 	div $t0,$t0,0x80
@@ -172,7 +162,7 @@ gettailXY:				#get tailXY
 	mflo $t5                         #get headY
 	div $t4,$t0,4			 #get headX
 
-	#================real Strategy====================
+	#================security2====================
 	
 	slt $t0,$s4,$s6          		 
 	bne $t0,$zero,foodXrightofsnakeX    	# hx > foodX
@@ -188,7 +178,7 @@ foodXleftofsnakeX:
 foodlloop:
 	beq $s4,$s6,strateEND			#if already eat food goto strateEND(reset head coordinate)
 	addi $s4,$s4,-1				#goto left
-	j left
+	j leftS
 
 foodXrightofsnakeX:	
 	slt $t0,$s5,$s7        
@@ -198,35 +188,29 @@ foodXrightofsnakeX:
 foodrloop:	
 	beq $s4,$s6,strateEND		#if already eat food goto strateEND(reset head coordinate)
 	addi $s4,$s4,1			#goto right
-	j right
+	j rightS
 headYsmallfoodY:
 	addi $s5,$s5,1			#goto under(because food is under than snake)
-	j under
+	j underS
 	
 headYbigerfoodY:
 	addi $s5,$s5,-1			#goto up(because food is up than snake)
-	j up
+	j upS
 
 strateEND:
-	#move $t9,$s5
-	#move $t8,$s4
-	j headXY			#reset head coordinate
+	j FindheadXY			#reset head coordinate
 	
-	#lw $t0,0xffff0000 	
-	#blez $t0, Direction 
-	#lw $a3,0xffff0004
-	#################################################################	
+#################################################################	
 #execution(NEW snake head address)
 underS:						
-	la $s0, snakeaddress		#Load the head address to $s0
+	la $s0, snakeaddr		#Load the head address to $s0
 	lw $s2,($s0)			#Load old head address to $s2
 	addi $s1,$s2,128		#New head address to $s1
-	
 	lw $t0,snakecolor		#load snake color
 	lw $t1,($s1)			#load next step's color
-	beq $t1,$t0,Ls1			# 若下一步會吃到自己，則轉向
+	beq $t1,$t0,Ls1			# 若下一步會吃到自己，轉向
 	lw $t0,contourcolor		#load contour color
-	beq $t1,$t0,Ls1			# 若下一步會出界，則轉向
+	beq $t1,$t0,Ls1			# 若下一步會出界，轉向
 	beq $t7,$0,judgeS		# t7 是一個旗標，用於確認是否已經因為下一步會吃到自己而轉向走其他方向，若又則t7會為1	
 	li $t7,0			#重設旗標t7
 	addi $s5,$s5,1			#因為多走了這部，所以要將插值調整回去，讓hx,hy,foodx,foody的距離差正確
@@ -240,15 +224,14 @@ Ls1:
 	addi $s5,$s5,-1
 	j rightS
 rightS:
-	la $s0, snakeaddress		#Load the head address to $s0
-	lw $s2,($s0)			#Load old head address
+	la $s0, snakeaddr		#加載舊頭的地址 to $s0
+	lw $s2,($s0)			#加載舊頭的地址
 	addi $s1,$s2,4			#New head address
-	
 	lw $t0,snakecolor		#load snake color
 	lw $t1,($s1)			#load next step's color
-	beq $t1,$t0,Ls2			# 若下一步會吃到自己，則轉向
+	beq $t1,$t0,Ls2			# 若下一步會吃到自己，轉向
 	lw $t0,contourcolor		#load contour color
-	beq $t1,$t0,Ls2			# 若下一步會出界，則轉向
+	beq $t1,$t0,Ls2			# 若下一步會出界，轉向
 	beq $t7,$0,judgeS		# t7 是一個旗標，用於確認是否已經因為下一步會吃到自己而轉向走其他方向，若又則t7會為1
 	li $t7,0			#重設旗標t7
 	addi $s4,$s4,1			#因為多走了這部，所以要將插值調整回去，讓hx,hy,foodx,foody的距離差正確
@@ -262,19 +245,18 @@ Ls2:
 	addi $s4,$s4,-1
 	j upS
 leftS:
-	la $s0, snakeaddress		#Load the head address to $s0
-	lw $s2,($s0)			#Load old head address
-	subi $s1,$s2,4			#New head address
-	
+	la $s0, snakeaddr		#Load the head address to $s0
+	lw $s2,($s0)			#加載舊頭的地址
+	subi $s1,$s2,4			#新頭的地址
 	lw $t0,snakecolor		#load snake color
 	lw $t1,($s1)			#load next step's color
-	beq $t1,$t0,Ls3			# 若下一步會吃到自己，則轉向
+	beq $t1,$t0,Ls3			# 若下一步會吃到自己，轉向
 	lw $t0,contourcolor		#load contour color
-	beq $t1,$t0,Ls3			# 若下一步會出界，則轉向
+	beq $t1,$t0,Ls3			# 若下一步會出界，轉向
 	beq $t7,$0,judgeS		# t7 是一個旗標，用於確認是否已經因為下一步會吃到自己而轉向走其他方向，若又則t7會為1
 	li $t7,0			#重設旗標t7
 	addi $s4,$s4,-1			#因為多走了這部，所以要將插值調整回去，讓hx,hy,foodx,foody的距離差正確
-	li $t6,15			#用於判斷是否進入死亡迴圈
+	li $t6,15			#用於判斷是否進入了死亡迴圈
 	j judgeS			#goto judge
 Ls3:
 	addi $t6,$t6,-1
@@ -284,18 +266,17 @@ Ls3:
 	addi $s4,$s4,1
 	j underS
 upS:
-	la $s0, snakeaddress		#Load the head address to $s0
-	lw $s2,($s0)			#Load old head address
-	subi $s1,$s2,128		#New head address
-	
+	la $s0, snakeaddr		#加載舊頭的地址 to $s0
+	lw $s2,($s0)			#加載舊頭的地址
+	subi $s1,$s2,128		#新頭的地址
 	lw $t0,snakecolor		#load snake color
 	lw $t1,($s1)			#load next step's color
-	beq $t1,$t0,Ls4			# 若下一步會吃到自己，則轉向
+	beq $t1,$t0,Ls4			# 若下一步會吃到自己，轉向
 	lw $t0,contourcolor		#load contour color
-	beq $t1,$t0,Ls4			# 若下一步會出界，則轉向
+	beq $t1,$t0,Ls4			# 若下一步會出界，轉向
 	beq $t7,$0,judgeS		# t7 是一個旗標，用於確認是否已經因為下一步會吃到自己而轉向走其他方向，若又則t7會為1
 	li $t7,0			#重設旗標t7
-	addi $s5,$s5,-1			#因為多走了這部，所以要將插值調整回去，讓hx,hy,foodx,foody的距離差正確
+	addi $s5,$s5,-1			#多走了這步，所以要將插值調整回去，讓hx,hy,foodx,foody的距離之差正確
 	li $t6,15			#用於判斷是否進入死亡迴圈
 	j judgeS			#goto judge
 	
@@ -309,33 +290,30 @@ Ls4:
   ##################################################################
 judgeS:
 	
-	jal  check_finalS
-	lw	$t0,($s1)		#Load color occupied by the next head address in ($s1)
+	jal  final_check
+	lw	$t0,($s1)		#加載（$ s1）中下一個頭地址佔用的color
 	lw $t3,foodcolor		#Load foodcolor to $t3
-	beq $t0,$t3,growupS		#If I eat the food, I grow up
+	beq $t0,$t3,gupS		#If I eat the food, I grow up
 snake_moveS:	
 	lw $t0,snakecolor		#Load snakecolour to $t0
 	lw $s3,bodylength		#Load bodylength to $s3
 	sw $t0,($s1)			#Colour new head
-	sw $s1,($s0)			#Store new head address to "snakeaddress"
+	sw $s1,($s0)			#Store new head address to "snakeaddr"
 	jal snakebody_shiftS
-	j  StrategyS
+	j  SecurityS
 
 
 
 finalS:
 	li $s3,3			#Reset bodylength to 3
-	sh $s3,bodylength		
-	#lw $a3,0x10010000
+	sh $s3,bodylength	
 	li $a0,500			#Sleep for 0.5s when dead
 	li $v0,10
 	syscall
 	j init			#Return to the game
 
-#function
-################################################################################
-################################################################################
-#Clear the screen (initialize the bitmap)-----------------------------------------------------
+#常駐功能#########################################################
+#Clear the screen (initialize the bitmap)--------------
 clear:
 	lw $t0,startaddress		#startaddress = 0x10008000
 	lh $t1, side			#Load the side of the bitmap
@@ -349,7 +327,7 @@ clearloop:					#Loop until every display address has the black number in it (zer
 	jr $ra				#back to mStrategyn
 
 	
-#This function is used to draw both the up row and the bottom row-------------------------------
+#This function is used to draw both the up row and the bottom row-------------
 row:								
 	lw $t1, contourcolor		#Load contourcolor
 	lh $t2, side			#Load the side of the bitmap
@@ -361,7 +339,7 @@ drawrow:					#Row loop
 	jr $ra				#back to mStrategyn
 	
 	
-#Draw the columns--------------------------------------------------------------------------------	
+#Draw the columns-------------------
 columns:						
 	lw $t0, startaddress		#Load the start address
 	addi $t0,$t0,128		#Go to the next line: add 0x80
@@ -377,15 +355,15 @@ drawcolumns:				#Columns Loop
 	jr $ra				#back to mStrategyn
 	
 	
-#Draw the snake-----------------------------------------------------------------------------------
+#Draw the snake-----------------
 snake:					
-	la $s0,snakeaddress		#Load the address of "snakeaddress"
+	la $s0,snakeaddr		#Load the address of "snakeaddress"
 	lw $s3,bodylength		#Load bodylength
 	lw $t0,snakecolor		#Load snakecolor
-	lw $t1,($s0)			#Load the address of snake's head which was saved in "snakeaddress" to $t1
+	lw $t1,($s0)			#Load the address of snake's head which was saved in "snakeaddr" to $t1
 drawsnake:				#Snake loop
 	sw $t0,($t1)			#Store snake color
-	sw $t1,($s0)			#Store next bodypart address to "snakeaddress" array
+	sw $t1,($s0)			#Store next bodypart address to "snakeaddr" array
 	subi $t1,$t1,4			#Move $t1 to the next bodypart address (the next bodypart is on the left of the head)
 	addi $s0,$s0,4			#Move to the next element of "snakeaddress" array
 	subi $s3,$s3,1			#bodylength = bodylength - 1
@@ -393,7 +371,7 @@ drawsnake:				#Snake loop
 	jr $ra				#back to mStrategyn
 	
 	
-#Draw the food with  a pseudorandom address----------------------------------------------------------
+#Draw the food with  a pseudorandom address----------------
 food:						
 	li $a1,0x3c0			#$a1= 32 pixel * (32 - 2)pixel ; food will be randomly gernerate within 0x10008080 ~ (0x10008F80 - 4)
 	li $v0,42			#Generate a random number to $a0 ( from 0 to value($a1) )
@@ -401,26 +379,21 @@ food:
 	sll $a0, $a0, 2			#Multiply $a0 with 4 to generate the address (4 bytes for each pixel)
 	add $a0, $a0,0x10008000		#Put the random number on the bitmap address
 	lw $t0, ($a0)			#And then check if the new address is already colored
-	#lw $t1,snakecolor
-	#lw $t2,contourcolor
 	bnez $t0,food
 	lw $t1,foodcolor		#Load foodcolor
 	sw $t1,($a0)			#Finally draw the food
 foodXY:  #Calculate food's coordinate
-	subi $t0,$a0,0x10008000		#food.X = ( foodaddress - 0x10008000 ) % 0x80 /4
+	subi $t0,$a0,0x10008000	#food.X = ( foodaddress - 0x10008000 ) % 0x80 /4
 	div $t0,$t0,0x80
 	mfhi $t0			
 	mflo $s7			#food.Y = ( foodaddress - 0x10008000) / 0x80	
 	div $s6,$t0,4			#$s6 = food.X
-	#add $t9,$s7,$0
-	#add $t8,$s6,$0
 	jr $ra
-########################################################################################################	
-#"judge" use function
-########################################################################################################
-########################################################################################################
-#----------------------------------------------------------------------------------------------
-check_finalS:			
+#####################################################
+#"judge"用的功能
+#####################################################
+#-------------------------------------
+final_check:			
 	lw	$t0,($s1)		#Load color occupied by the next head address in ($s1)
 	lw $t1,contourcolor		#Load contourcolor to $t1
 	lw $t2,snakecolor		#Load snakecolor to $t2
@@ -428,11 +401,8 @@ check_finalS:
 	beq $t0,$t2,finalS		#If I bit myself, I lose
 	jr $ra				#back to common
 
-#snake growup -----------------------------------------------------------------------------------	
-growupS:		
-	
-	#li $s6,1			
-	#sw $s6,growupflag
+#snake growup ----------------------	
+gupS:	
 	lw $s3,bodylength		
 	addi $s3,$s3,1			#Increase bodylength
 	sw $s3, bodylength		#Store new bodylength
@@ -440,7 +410,7 @@ growupS:
 	sw $t0,($s1)			#color new head
 	lw $s2,($s0)			#Load old head address
 	sw $s1,($s0)			#Store new head address to "snakeaddress"	
-#print message-----------------------------------------
+#print message--------------------
 	li $v0,4
 	la $a0, outstring
 	syscall
@@ -450,9 +420,8 @@ growupS:
 	li $v0,4
 	la $a0, newline
 	syscall
-#--------------------------------------------------------
-	jal food				#in initialization use function
-		
+#-----------------------------
+	jal food						
 	j	snake_moveS
 	
 	
@@ -469,5 +438,6 @@ snakebody_shiftS:
 	jr $ra				#back to growup or common	
 
 	
-	
-############################################################################################################
+#########################################################################
+
+
